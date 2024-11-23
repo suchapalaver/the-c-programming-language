c89b0c2df1fc0957335969cbbb1eed307c456380
Joseph Livesey <jlivesey@gmail.com>
2024-11-23 17:27:50 -0500

    chore: add count characters version 2

--- count-characters.c
+++ count-characters.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input; 2nd version */
 int main() {
-  long nc;
+  double nc;
 
-  nc = 0;
-  while (getchar() != EOF) {
-    ++nc;
+  for (nc = 0; getchar() != EOF; ++nc) {
+    ;
   }
-  printf("%ld\n", nc);
+  printf("%.0f\n", nc);
 }
