9a76c15c69e69c4919421d6d2b87b8cd21c500d3
Joseph Livesey <jlivesey@gmail.com>
2024-11-23 15:56:26 -0500

    chore(fahrenheit-celcius): right-justify numbers

--- fahrenheit-celcius-table.c
+++ fahrenheit-celcius-table.c
@@ -13,7 +13,7 @@ int main() {
   fahr = lower;
   while (fahr <= upper) {
     celcius = 5 * (fahr - 32) / 9;
-    printf("%d\t%d\n", fahr, celcius);
+    printf("%3d %6d\n", fahr, celcius);
     fahr = fahr + step;
   }
 }
