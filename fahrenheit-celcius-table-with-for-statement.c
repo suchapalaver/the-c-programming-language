952dc968d2cc513b7e295319bb35d72c6c13a3a3
Joseph Livesey <jlivesey@gmail.com>
2024-11-23 16:41:25 -0500

    chore: define symbolic constants for 'magic numbers'

--- fahrenheit-celcius-table-with-for-statement.c
+++ fahrenheit-celcius-table-with-for-statement.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LOWER 0   /* lower limit of termperature table */
+#define UPPER 300 /* upper limit */
+#define STEP 20   /* step size */
+
 /* print Fahrenheit-Celcius table */
 int main() {
   int fahr;
 
-  for (fahr = 300; fahr >= 0; fahr -= 20)
+  for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
     printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
